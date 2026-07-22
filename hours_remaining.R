14c0fdb74408a5703a0a4112403696eeb4f774f3
Clay Glad <clay@testive.com>
2026-07-22 17:15:05 -0400

    Found correct tables for download in Oases

--- hours_remaining.R
+++ hours_remaining.R
@@ -1,31 +1,21 @@
-# ostudents = download Student Hours from oasesonline.com.
-# oparents = download Other Area/Parent from oasesonline.com.
-# These can be downloaded only as .xlsx. Must be converted to .csv
+# ostudents = download Student Details from oasesonline.com.
+# ohours = download Student Invoiced Hours from oasesonline.com.
+# These can be downloaded only as .xlsx. Must be converted to .csv.
 
 source('~/Testive/db_testive/basic_tools.R')
 
-hhmm_to_decimal <- function(x) {
-  parts <- do.call(rbind, strsplit(x, ":"))
-  hours <- as.numeric(parts[, 1])
-  minutes <- as.numeric(parts[, 2])
-  hours + minutes / 60
-}
+ohours = read.csv('~/Desktop/oases_hours.csv') %>%
+  filter(Hours.Invoiced > 0) %>%
+  rename(Hours.Remaining = Hours.Left) %>%
+  unite('Student', Lastname:Firstname, sep = ', ') %>%
+  select(Student, Session.Type, Hours.Remaining)
 
-ostudents = read.csv('~/Desktop/ostudents.csv') %>%
-  mutate(Hours.Left = hhmm_to_decimal(Hours.Left)) %>%
-  filter(grepl('Test Prep', District) &
-           Status == 'Active' &
-           Hours.Left <= 3 &
-           !grepl('Fake', Student)) %>%
-  select(Student, Tutor, Student.ID, Hours.Left)
+ostudents = read.csv('~/Desktop/oases_students.csv') %>%
+  select(Student, Tutor, Parent, Parent.Cell, Email) %>%
+  rename(Parent.Email = Email)
 
-oparents = read.csv('~/Desktop/oparents.csv') %>%
-  filter(Student.ID %in% ostudents$Student.ID) %>%
-  select(Parent, Email, Student.ID)
-
-remaining = left_join(ostudents, oparents) %>%
-  select(-Student.ID) %>%
-  arrange(Hours.Left) %>%
-  distinct(.)
+remaining = left_join(ohours, ostudents) %>%
+  filter(Hours.Remaining < 5) %>%
+  arrange(Hours.Remaining)
 
 write.csv(file = '~/Desktop/oremaining.csv', remaining, row.names = FALSE)
